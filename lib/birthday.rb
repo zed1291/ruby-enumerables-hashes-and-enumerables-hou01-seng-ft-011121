e22b997405ef19123ee90e4f38f9881b6184e338
zed <zed@kolk.me>
2020-12-24 02:13:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,6 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |name, age|
-    "Happy Birthday #{name}! You are now #{age} years old!"
+    p "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
